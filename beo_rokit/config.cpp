843dbd26af22e441a3422846f139abc5c0b54df6
Walker <cw_rocks@hotmail.com>
2023-04-09 09:11:04 +0100

    ROKIT Updates to align with public mod

--- beo_rokit/config.cpp
+++ beo_rokit/config.cpp
@@ -14,18 +14,12 @@ class CfgPatches
 			"beo_arty_rokit",
 			"beo_arty_rokit_opf",
 			"beo_arty_rokit_ind",
-			"beo_arty_rokit_he",
-			"beo_arty_rokit_he_opf",
-			"beo_arty_rokit_he_ind",
 			"beo_arty_rokit_cluster",
 			"beo_arty_rokit_cluster_opf",
 			"beo_arty_rokit_cluster_ind",
 			"beo_arty_hilux_arty",
 			"beo_arty_hilux_arty_opf",
 			"beo_arty_hilux_arty_ind",
-			"beo_arty_hilux_arty_he",
-			"beo_arty_hilux_arty_he_opf",
-			"beo_arty_hilux_arty_he_ind",
 			"beo_arty_hilux_arty_cluster",
 			"beo_arty_hilux_arty_cluster_opf",
 			"beo_arty_hilux_arty_cluster_ind"
@@ -33,9 +27,9 @@ class CfgPatches
 			
 			};
 		weapons[] = {};
-		requiredAddons[] = {"A3_Weapons_F","A3_Weapons_F_Mark","A3_Weapons_F_Exp","beo_wps","beo_ai","uk3cb_factions_vehicles_hilux"};
-		version = "1.0";
-		requiredVersion = "1.0";
+		requiredAddons[] = {"A3_Weapons_F","A3_Weapons_F_Mark","A3_Weapons_F_Exp","ace_csw","beo_wps","beo_ai","uk3cb_factions_vehicles_hilux"};
+		version = "1.1";
+		requiredVersion = "1.1";
 		author = "Walker";
 	};
 };
@@ -101,14 +95,41 @@ class CfgVehicles
 		faction = "beo_ai_blu";
 		crew = "beo_ai_crew";
 		typicalCargo[] = {"beo_ai_crew"};
+		hiddenSelectionsTextures[] = {"\beo_rokit\data\rokit_mk6.paa"};
 		class Turrets: Turrets 
 		{		
 			class MainTurret: MainTurret
 			{
-				magazines[] = {"beo_arty_mag12_122_he","beo_arty_mag12_122_he","beo_arty_mag12_122_he","beo_arty_mag12_122_he","beo_arty_mag12_122_cluster","beo_arty_mag12_122_cluster"};
+				magazines[] = {"beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he","beo_arty_mag_122_he"};
 				weapons[] = {"beo_arty_rokit"};
 			};
 		};	
+
+		class assembleInfo
+		{
+			primary=0;
+			base="";
+			assembleTo="";
+			dissasembleTo[]=
+			{
+				"beo_rokit_bag",
+				"beo_rokit_bipod_bag"
+			};
+			displayName="";
+		};	
+		class ACE_CSW 
+		{
+			enabled = 1; // Enables ACE CSW for this weapon              
+			proxyWeapon = "beo_arty_rokit_proxy"; // The proxy weapon created above
+			magazineLocation="_target selectionPosition 'usti hlavne'"; // Ammo handling interaction point location
+			disassembleWeapon = "beo_arty_rokit_carry";  // Carryable weapon created above
+			disassembleTurret = "ace_csw_mortarBaseplate";  // Which static tripod will appear when weapon is disassembled
+			ammoLoadTime = 7;   // How long it takes in seconds to load ammo into the weapon           
+			ammoUnloadTime = 5; // How long it takes in seconds to unload ammo from the weapon
+			desiredAmmo = 1;  // When the weapon is reloaded it will try and reload to this ammo capacity
+			// Optional callback function for when the CSW gets disassembled, called with [tripod, staticWeapon]
+			disassembleFunc = "prefix_fnc_handleDisassembly";
+		};
 	};	
 	
 	class beo_arty_rokit_opf: beo_arty_rokit
@@ -134,6 +155,7 @@ class CfgVehicles
 	{
 		displayname = "[BEO AI] ROKIT Artillery TEST";
 		model = "\beo_rokit\data\quassimLaunchertri.p3d";
+		hiddenSelectionsTextures[] = {};
 		
 		class Turrets: Turrets 
 		{		
@@ -153,8 +175,10 @@ class CfgVehicles
 
 	class beo_arty_rokit_he: beo_arty_rokit
 	{
-		scope = 2;		
+		//Hidden to depreciate
+		scope = 1;		
 		displayname = "[BEO AI] ROKIT Artillery (HE)";
+		hiddenSelectionsTextures[] = {"\beo_rokit\data\rokit_mk6.paa"};
 		class Turrets: Turrets 
 		{		
 			class MainTurret: MainTurret
@@ -167,7 +191,7 @@ class CfgVehicles
 	
 	class beo_arty_rokit_he_opf: beo_arty_rokit_he
 	{
-		scope = 2;
+		scope = 1;
 		side = 0;
 		faction = "beo_ai_opf";
 		crew = "beo_ai_crew_opf";
@@ -176,7 +200,7 @@ class CfgVehicles
 	
 	class beo_arty_rokit_he_ind: beo_arty_rokit_he
 	{
-		scope = 2;
+		scope = 1;
 		side = 2;
 		faction = "beo_ai_ind";
 		crew = "beo_ai_crew_ind";
@@ -184,17 +208,31 @@ class CfgVehicles
 	};
 
 
-	class beo_arty_rokit_cluster: beo_arty_rokit_he
+	class beo_arty_rokit_cluster: beo_arty_rokit
 	{
 		scope = 2;		
 		displayname = "[BEO AI] ROKIT Artillery (Cluster)";
+		hiddenSelectionsTextures[] = {"\beo_rokit\data\rokit_mk6.paa"};
 		class Turrets: Turrets 
 		{		
 			class MainTurret: MainTurret
 			{
-				magazines[] = {"beo_arty_mag12_122_cluster","beo_arty_mag12_122_cluster","beo_arty_mag12_122_cluster","beo_arty_mag12_122_cluster","beo_arty_mag12_122_cluster","beo_arty_mag12_122_cluster"};
+				magazines[] = {"beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster"};
 				weapons[] = {"beo_arty_rokit"};
 			};
+		};	
+
+		class assembleInfo
+		{
+			primary=0;
+			base="";
+			assembleTo="";
+			dissasembleTo[]=
+			{
+				"beo_rokit_cluster_bag",
+				"beo_rokit_bipod_bag"
+			};
+			displayName="";
 		};	
 	};	
 	
@@ -220,8 +258,143 @@ class CfgVehicles
 
 
 
+	class beo_arty_rokit_mlrs: beo_arty_rokit
+	{
+		scope = 2;		
+		displayname = "[BEO AI] ROKIT MLRS";
+		class Turrets: Turrets 
+		{		
+			class MainTurret: MainTurret
+			{
+				magazines[] = {"beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster","beo_arty_mag_122_cluster"};
+				weapons[] = {"beo_arty_rokit"};
+			};
+		};	
+
+		class assembleInfo
+		{
+			primary=0;
+			base="";
+			assembleTo="";
+			dissasembleTo[]=
+			{
+				"beo_rokit_mlrs_bag",
+				"beo_rokit_bipod_bag"
+			};
+			displayName="";
+		};	
+		class ACE_CSW 
+		{
+			enabled = 1; // Enables ACE CSW for this weapon              
+			proxyWeapon = "beo_arty_rokit_mlrs_proxy"; // The proxy weapon created above
+			magazineLocation="_target selectionPosition 'usti hlavne'"; // Ammo handling interaction point location
+			disassembleWeapon = "beo_arty_rokit_mlrs_carry";  // Carryable weapon created above
+			disassembleTurret = "ace_csw_mortarBaseplate";  // Which static tripod will appear when weapon is disassembled
+			ammoLoadTime = 12;   // How long it takes in seconds to load ammo into the weapon           
+			ammoUnloadTime = 10; // How long it takes in seconds to unload ammo from the weapon
+			desiredAmmo = 10;  // When the weapon is reloaded it will try and reload to this ammo capacity
+			// Optional callback function for when the CSW gets disassembled, called with [tripod, staticWeapon]
+			disassembleFunc = "prefix_fnc_handleDisassembly";
+		};
+	};	
+	
+	class beo_arty_rokit_mlrs_opf: beo_arty_rokit_mlrs
+	{
+		scope = 2;
+		side = 0;
+		faction = "beo_ai_opf";
+		crew = "beo_ai_crew_opf";
+		typicalCargo[] = {"beo_ai_crew_opf"};
+	};
+	
+	class beo_arty_rokit_mlrs_ind: beo_arty_rokit_mlrs
+	{
+		scope = 2;
+		side = 2;
+		faction = "beo_ai_ind";
+		crew = "beo_ai_crew_ind";
+		typicalCargo[] = {"beo_ai_crew_ind"};
+	};
 
 
+
+	// ROKIT Backpacks
+
+	class B_Mortar_01_weapon_F;
+	class I_Mortar_01_weapon_F: B_Mortar_01_weapon_F
+	{
+		class assembleInfo;
+	};
+	class B_Mortar_01_support_F;
+	class I_Mortar_01_support_F: B_Mortar_01_support_F
+	{
+		class assembleInfo;
+	};	
+
+	class beo_rokit_bag: I_Mortar_01_weapon_F
+	{
+        scope = 2;
+		author = "Walker";
+		displayName = "ROKIT Launcher Bag";
+		mass = 150;
+		class assembleInfo: assembleInfo
+		{
+			displayName = "ROKIT Launcher";
+			assembleTo = "beo_arty_rokit";
+			base[] = {"beo_rokit_bipod_bag"};
+		};
+		faction = "BLU_F";
+		editorSubcategory = "EdSubcat_DismantledWeapons";
+	};
+
+	class beo_rokit_cluster_bag: I_Mortar_01_weapon_F
+	{
+        scope = 2;
+		author = "Walker";
+		displayName = "ROKIT Launcher (Cluster) Bag";
+		mass = 150;
+		class assembleInfo: assembleInfo
+		{
+			displayName = "ROKIT (Cluster) Launcher";
+			assembleTo = "beo_arty_rokit_cluster";
+			base[] = {"beo_rokit_bipod_bag"};
+		};
+		faction = "BLU_F";
+		editorSubcategory = "EdSubcat_DismantledWeapons";
+	};
+
+	class beo_rokit_mlrs_bag: I_Mortar_01_weapon_F
+	{
+        scope = 2;
+		author = "Walker";
+		displayName = "ROKIT MLRS Bag";
+		mass = 150;
+		class assembleInfo: assembleInfo
+		{
+			displayName = "ROKIT MLRS";
+			assembleTo = "beo_arty_rokit_mlrs";
+			base[] = {"beo_rokit_bipod_bag"};
+		};
+		faction = "BLU_F";
+		editorSubcategory = "EdSubcat_DismantledWeapons";
+	};
+
+	class beo_rokit_bipod_bag: I_Mortar_01_support_F
+	{
+		scope = 2;
+		mass = 120;
+		author = "Walker";
+		displayName = "ROKIT Bipod Bag";
+		faction = "BLU_F";
+		class assembleInfo: assembleInfo
+		{
+			primary = 0;
+			base = "";
+			assembleTo = "";
+			dissasembleTo[] = {};
+			displayName = "";
+		};
+	};
 
 
 
@@ -278,7 +451,8 @@ class CfgVehicles
 
 	class beo_arty_hilux_arty_he: UK3CB_TKM_I_Hilux_Rocket_Arty
 	{	
-		scope = 2;
+		//hidden as depreciated
+		scope = 1;
 		side = 1;
 		vehicleClass = "rhs_vehclass_artillery";
 		editorSubcategory = "rhs_EdSubcat_artillery";
@@ -300,7 +474,7 @@ class CfgVehicles
 	
 	class beo_arty_hilux_arty_he_opf: beo_arty_hilux_arty_he
 	{
-		scope = 2;
+		scope = 1;
 		side = 0;
 		faction = "beo_ai_opf";
 		crew = "beo_ai_crew_opf";
@@ -309,7 +483,7 @@ class CfgVehicles
 	
 	class beo_arty_hilux_arty_he_ind: beo_arty_hilux_arty_he
 	{
-		scope = 2;
+		scope = 1;
 		side = 2;
 		faction = "beo_ai_ind";
 		crew = "beo_ai_crew_ind";
@@ -593,54 +767,236 @@ class CfgWeapons
 			"beo_arty_mag10_122_cluster"
 		};
 	};
+
+
+
+    class beo_arty_rokit_proxy: beo_arty_rokit 
+	{
+        magazineReloadTime = 6;
+    };
+
+    class beo_arty_rokit_mlrs_proxy: beo_arty_rokit_mlrs 
+	{
+        magazineReloadTime = 12;
+    };
+
+   	class Launcher;
+    class Launcher_Base_F: Launcher 
+	{
+        class WeaponSlotsInfo;
+    };
+
+  class beo_arty_rokit_carry: Launcher_Base_F 
+	{
+        // displayName, author, model and picture are omitted and up to you
+		displayName = "[CSW] ROKIT Launcher"
+		author = "Walker";
+		model="\z\ace\addons\apl\ACE_CSW_Bag.p3d";
+		modes[]={};
+		picture="\z\ace\addons\csw\UI\StaticMortarTube_Icon.paa";
+        scope = 2;
+        modes[] = {};
+
+        class ACE_CSW 
+		{
+            type = "weapon"; // Use "weapon" for weapons or "mount" for tripods - see below
+            deployTime = 10;  // How long it takes to deploy the weapon onto the tripod
+            pickupTime = 10;  // How long it takes to disassemble weapon from the tripod
+            class assembleTo {
+                // What tripod can this weapon deploy onto, and what vehicle will it spawn when it is deployed
+                ace_csw_mortarBaseplate = "beo_arty_rokit";
+            };
+        };
+
+        class WeaponSlotsInfo: WeaponSlotsInfo 
+		{
+            // One WeaponSlot with a positive value for iconScale forces game to use icon overlay method
+            // Required, because the inventory icon has no accessory variants
+            class MuzzleSlot 
+			{
+                iconScale = 0.1;
+            };
+
+            // Don't forget to set mass to a reasonable value
+            // We use mass in pounds * 10
+            mass = 150; // 84 lb / 38 kg
+        };
+    };	
+
+    class beo_arty_rokit_mlrs_carry: Launcher_Base_F 
+	{
+        // displayName, author, model and picture are omitted and up to you
+		displayName = "[CSW] ROKIT MLRS"
+		author = "Walker";
+		model="\z\ace\addons\apl\ACE_CSW_Bag.p3d";
+		modes[]={};
+		picture="\z\ace\addons\csw\UI\StaticMortarTube_Icon.paa";
+        scope = 2;
+        modes[] = {};
+
+        class ACE_CSW 
+		{
+            type = "weapon"; // Use "weapon" for weapons or "mount" for tripods - see below
+            deployTime = 15;  // How long it takes to deploy the weapon onto the tripod
+            pickupTime = 15;  // How long it takes to disassemble weapon from the tripod
+            class assembleTo {
+                // What tripod can this weapon deploy onto, and what vehicle will it spawn when it is deployed
+                ace_csw_mortarBaseplate = "beo_arty_rokit_mlrs";
+            };
+        };
+
+        class WeaponSlotsInfo: WeaponSlotsInfo 
+		{
+            // One WeaponSlot with a positive value for iconScale forces game to use icon overlay method
+            // Required, because the inventory icon has no accessory variants
+            class MuzzleSlot 
+			{
+                iconScale = 0.1;
+            };
+
+            // Don't forget to set mass to a reasonable value
+            // We use mass in pounds * 10
+            mass = 200; // 84 lb / 38 kg
+        };
+    };	
 	
 };
 
 class cfgMagazines
 {
-
 	class 12Rnd_230mm_rockets;
-	class beo_arty_mag12_122_he: 12Rnd_230mm_rockets
+	class beo_arty_mag_122_he: 12Rnd_230mm_rockets
 	{
 		scope = 2;
 		ammo = "beo_arty_ammo_122_r_he";
-		displayName = "[BEO AI] 12x 122mm HE Rocket";
-		count = 12;
-		initSpeed = 400;//850
+		displayName = "122mm HE ROKIT";
+		displayNameShort="122mm HE ROKIT";
+		count = 1;
+		initSpeed = 400;
+		picture="\A3\Weapons_F\launchers\RPG32\data\UI\gear_rpg32_rocket_ca.paa";
 	};	
-
-	class beo_arty_mag12_122_test: beo_arty_mag12_122_he
+	class beo_arty_mag10_122_he: beo_arty_mag_122_he
 	{
 		scope = 2;
-		ammo = "beo_arty_ammo_122_r_test";
-		displayName = "[BEO AI] 12x 122mm TEST Rocket";
-	};
-	
-	class beo_arty_mag10_122_he: beo_arty_mag12_122_he
-	{
-		scope = 2;
-		displayName = "[BEO AI] 10x 122mm HE Rocket";
+		displayName = "10x 122mm HE ROKIT";
+		displayNameShort="122mm HE ROKIT";
 		count = 10;		
+		picture="\A3\Weapons_F\launchers\RPG32\data\UI\gear_rpg32_rocket_ca.paa";
 	};	
 
 	
-	class beo_arty_mag12_122_cluster: 12Rnd_230mm_rockets
+	class beo_arty_mag_122_cluster: 12Rnd_230mm_rockets
 	{
 		scope = 2;
 		ammo = "beo_arty_ammo_122_r_cluster";
-		displayName = "[BEO AI] 12x 122mm Cluster Rocket";
-		count = 12;
-		initSpeed = 400;//850
+		displayName = "122mm Cluster ROKIT";
+		displayNameShort="122mm Cluster ROKIT";
+		count = 1;
+		initSpeed = 400;
+		picture="\A3\Weapons_F\launchers\RPG32\data\UI\gear_rpg32_rocket_ca.paa";
 	};	
 	
-	class beo_arty_mag10_122_cluster: beo_arty_mag12_122_cluster
+	class beo_arty_mag10_122_cluster: beo_arty_mag_122_cluster
 	{
 		scope = 2;
-		displayName = "[BEO AI] 10x 122mm Cluster Rocket";
+		displayName = "10x 122mm Cluster ROKIT";
+		displayNameShort="122mm Cluster ROKIT";
 		count = 10;		
+		picture="\A3\Weapons_F\launchers\RPG32\data\UI\gear_rpg32_rocket_ca.paa";
 	};	
 	
+
+ // ace CSW
+
+    class beo_arty_mag_122_he_csw_mag: beo_arty_mag_122_he 
+	{
+        scope = 2;
+		scopeCurator=2;
+		ace_arsenal_hide=-1;
+		displayname="[CSW] 122mm HE ROKIT";
+        type = 256;  // Must be 256 to show up in Arsenal
+        count = 1;
+        //model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
+		model = "\A3\Weapons_F\Ammo\Rocket_01_F";
+		picture="\A3\Weapons_F\launchers\RPG32\data\UI\gear_rpg32_rocket_ca.paa";
+        ACE_isBelt = 0; // Enables belt linking for this magazine
+		mass=50;
+    };
+
+    class beo_arty_mag_122_cluster_csw_mag: beo_arty_mag_122_cluster 
+	{
+        scope = 2;
+		scopeCurator=2;
+		ace_arsenal_hide=-1;
+		displayname="[CSW] 122mm Cluster ROKIT";
+        type = 256;  // Must be 256 to show up in Arsenal
+        count = 1;
+        //model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
+		model = "\A3\Weapons_F\Ammo\Rocket_01_F";
+		picture="\A3\Weapons_F\launchers\RPG32\data\UI\gear_rpg32_rocket_ca.paa";
+        ACE_isBelt = 0; // Enables belt linking for this magazine
+		mass=50;
+    };
+
+    class beo_arty_mag10_122_csw_mag: beo_arty_mag10_122_he 
+	{
+        scope = 2;
+		scopeCurator=2;
+		ace_arsenal_hide=-1;
+		displayname="[CSW] 10x 122mm HE ROKIT";
+        type = 256;  // Must be 256 to show up in Arsenal
+        count = 1;
+        //model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
+		model = "\A3\Weapons_F\Ammo\Rocket_01_F";
+		picture="\A3\Weapons_F\launchers\RPG32\data\UI\gear_rpg32_rocket_ca.paa";
+        ACE_isBelt = 0; // Enables belt linking for this magazine
+		mass=100;
+    };
+    class beo_arty_mag10_122_cluster_csw_mag: beo_arty_mag10_122_cluster 
+	{
+        scope = 2;
+		scopeCurator=2;
+		ace_arsenal_hide=-1;
+		displayname="[CSW] 10x 122mm Cluster ROKIT";
+        type = 256;  // Must be 256 to show up in Arsenal
+        count = 1;
+        //model = "\A3\Structures_F_EPB\Items\Military\Ammobox_rounds_F.p3d";
+		model = "\A3\Weapons_F\Ammo\Rocket_01_F";
+		picture="\A3\Weapons_F\launchers\RPG32\data\UI\gear_rpg32_rocket_ca.paa";
+        ACE_isBelt = 0; // Enables belt linking for this magazine
+		mass=100;
+    };   
+
+
+
 };
+
+
+class ACE_CSW_Groups 
+{
+
+    class beo_arty_mag_122_he_csw_mag 
+	{ // Same name as the carryable magazine
+        beo_arty_mag_122_he = 1;    // Vehicle magazine that will be loaded when loading this magazine
+    };
+
+    class beo_arty_mag_122_cluster_csw_mag 
+	{ // Same name as the carryable magazine
+        beo_arty_mag_122_cluster = 1;    // Vehicle magazine that will be loaded when loading this magazine
+    };
+
+    class beo_arty_mag10_122_csw_mag 
+	{ // Same name as the carryable magazine
+        beo_arty_mag10_122_he = 10;    // Vehicle magazine that will be loaded when loading this magazine
+    };
+
+    class beo_arty_mag10_122_cluster_csw_mag 
+	{ // Same name as the carryable magazine
+        beo_arty_mag10_122_cluster = 10;    // Vehicle magazine that will be loaded when loading this magazine
+    };
+    
+};
+
 
 class cfgAmmo
 {
